1b46eafc0174ce14135888dc5f2d2bb50178ef3b
Michael <mikemach123@gmail.com>
2017-04-04 14:46:18 -0700

    google auth works

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,72 +1,50 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
-const API_KEY = "";
-const CLIENT_KEY = "";
+const API_KEY = "AIzaSyDipWSuBY5-BIMIcRsGVnbQ176Gv-2S6nI";
+const CLIENT_ID = "1068354047993-srsg6sa1srkam156q3897v9545r95f77.apps.googleusercontent.com";
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
 
+    this.initClient = this.initClient.bind(this);
+    this.handleAuthClick = this.handleAuthClick.bind(this);
+    this.handleSignoutClick = this.handleSignoutClick.bind(this);
   }
 
-  handleClientLoad() {
-    // Loads the client library and the auth2 library together for efficiency.
-    // Loading the auth2 library is optional here since `gapi.client.init` function will load
-    // it if not already loaded. Loading it upfront can save one network request.
-    gapi.load('client:auth2', initClient);
+  componentWillMount() {
+    gapi.load('client:auth2', this.initClient);
   }
 
   initClient() {
-    // Initialize the client with API key and People API, and initialize OAuth with an
-    // OAuth 2.0 client ID and scopes (space delimited string) to request access.
     gapi.client.init({
         apiKey: API_KEY,
         discoveryDocs: ["https://people.googleapis.com/$discovery/rest?version=v1"],
-        clientId: `${CLIENT_KEY}.apps.googleusercontent.com`,
+        clientId: CLIENT_ID,
         scope: 'profile'
-    }).then(function () {
-      // Listen for sign-in state changes.
-      gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
+    }).then(() => {
+          // Listen for sign-in state changes.
+          gapi.auth2.getAuthInstance().isSignedIn.listen(()=> {
+            console.log("google auth works");
+          });
 
-      // Handle the initial sign-in state.
-      updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-    });
+        });
   }
 
-  updateSigninStatus(isSignedIn) {
-    // When signin status changes, this function is called.
-    // If the signin status is changed to signedIn, we make an API call.
-    if (isSignedIn) {
-      makeApiCall();
-    }
-  }
-
-  handleSignInClick(event) {
-    // Ideally the button should only show up after gapi.client.init finishes, so that this
-    // handler won't be called before OAuth is initialized.
+  handleAuthClick(event) {
     gapi.auth2.getAuthInstance().signIn();
   }
 
-  handleSignOutClick(event) {
+  handleSignoutClick(event) {
     gapi.auth2.getAuthInstance().signOut();
-  }
-
-  makeApiCall() {
-    // Make an API call to the People API, and print the user's given name.
-    gapi.client.people.people.get({
-      resourceName: 'people/me'
-    }).then(function(response) {
-      console.log('Hello, ' + response.result.names[0].givenName);
-    }, function(reason) {
-      console.log('Error: ' + reason.result.error.message);
-    });
   }
 
   render() {
     return(
       <div className="home-page">
-
+        <button onClick={this.handleAuthClick}>Sign In</button>
+        <button onClick={this.handleSignoutClick}>Sign Out</button>
       </div>
     )
   }
