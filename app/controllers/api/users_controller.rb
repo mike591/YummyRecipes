0e07f60c05936f46d23c0a818aeb8be0bdd05de0
Michael <mikemach123@gmail.com>
2017-04-07 13:33:40 -0700

    add json for user and fix user col name

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,10 +1,29 @@
 class Api::UsersController < ApplicationController
   def create
+    @user = User.new(user_params)
+    if @user.save
+      render :show
+    else
+      render @user.errors.full_messages, status: 400
+    end
   end
 
   def show
+    @user = User.find_by(id: params[:id])
+    render :show
   end
 
   def update
+    @user = User.find_by(id: params[:id])
+    if @user.update(user_params)
+      render :show
+    else
+      render @user.errors.full_messages, status: 400
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :bio)
   end
 end
